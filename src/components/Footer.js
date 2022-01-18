686920e45d993fa2312ac946b31038dc1fd81cfe
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2022-01-18 11:56:51 +0100

    left component done

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,70 @@
-import React from 'react';
-import styled from 'styled-components'
+import React from "react";
+import { Col, Row, Image } from "react-bootstrap";
+import styled from "styled-components";
 
+const Container = styled.div`
+  fontfamily: "Twitter bold";
+  font-size: 15px;
+`;
 
-const Footer = () => {
+const Text = styled.p`
+  font-family: ${(props) =>
+    props.type === "name" ? "Twitter bold" : "Twitter"};
+  margin: 0;
+`;
+
+const Svg = styled.svg`
+  width: 18px;
+`;
+
+const Right = styled.div`
+  display: flex;
+  justify-content: flex-end;
+`
+
+const Footer = (props) => {
   return (
-    <div>
-      
-    </div>
+    <Container>
+      <Row
+        style={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Col xs={3}>
+          <Image roundedCircle="true"></Image>
+        </Col>
+        <Col xs={9}>
+          <Row
+            style={{
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "space-between",
+            }}
+          >
+            <Col>
+              <Text type="name">User</Text>
+              <Text type="username">@User</Text>
+            </Col>
+            <Col>
+              <Right>
+                <Svg
+                  viewBox="0 0 24 24"
+                  aria-hidden="true"
+                  class="r-18jsvk2 r-4qtqp9 r-yyyyoo r-1xvli5t r-dnmrzs r-bnwqim r-1plcrui r-lrvibr"
+                >
+                  <g>
+                    <circle cx="5" cy="12" r="2"></circle>
+                    <circle cx="12" cy="12" r="2"></circle>
+                    <circle cx="19" cy="12" r="2"></circle>
+                  </g>
+                </Svg>
+              </Right>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -2,11 +2,36 @@ import React from "react";
 import styled from "styled-components";
 import { Container, Nav, Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+
 import Sidebar from "../components/Sidebar";
+import Footer from "../components/Footer";
 
 const Main = styled.main`
   font-family: "Twitter";
   height: 100%;
+`;
+
+const FlexFooter = styled.div`
+  border-radius: 9999px;
+  border: none;
+  width: 255px;
+  margin-top: 4px;
+  margin-bottom: 4px;
+  padding: 10px;
+  min-width: 52px;
+  min-height: 52px;
+  cursor: pointer;
+  position: fixed;
+  bottom: 0;
+	transition-duration: 0.2s;
+
+  &:hover {
+    background-color: #e7e7e8;
+  }
+
+	@media (max-height: 334px) {
+		display: none;
+	}
 `;
 
 const Homepage = () => {
@@ -16,11 +41,13 @@ const Homepage = () => {
         <Row>
           <Col>
             <Sidebar />
+
+            <FlexFooter>
+              <Footer />
+            </FlexFooter>
           </Col>
-          <Col xs={5} style={{ backgroundColor: "blue" }}>
-          </Col>
-          <Col xs={4}  style={{ backgroundColor: "red" }}>
-          </Col>
+          <Col xs={5} style={{ backgroundColor: "blue" }}></Col>
+          <Col xs={4} style={{ backgroundColor: "red" }}></Col>
         </Row>
       </Container>
     </Main>
