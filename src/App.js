c29bebea1570f1056cf466b4364a18a554c341cd
SalwaRayan <90609792+SalwaRayan@users.noreply.github.com>
2022-01-19 10:45:28 +0100

    set pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 import GlobalFonts from './fonts/fonts'
 import Home from "./pages/Home";
 import Homepage from "./pages/Homepage";
+import Follow from "./pages/Follow";
+import Tweet from "./pages/Tweet";
+import User from "./pages/User";
+import UserList from "./pages/UserList";
 
 
 const App = () => {
@@ -14,6 +18,10 @@ const App = () => {
         <Routes>
           <Route exact path="/" element={<Home />} />
           <Route path="/homepage" element={<Homepage />} />
+          <Route path="/:idUser/user" element={<User />} />
+          <Route path="/:idUser/:idTweet/tweet" element={<Tweet />} />
+          <Route path="/users" element={<UserList />} />
+          <Route path="/:idUser/follow" element={<Follow />} />
         </Routes>
       </BrowserRouter>
     </>
